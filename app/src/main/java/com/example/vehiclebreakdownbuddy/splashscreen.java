4b713bf54635d0428e3882eae4d61b039b39b926
aditya <adityaatri9@gmail.com>
2023-08-20 19:47:44 +0530

    Commit!

--- app/src/main/java/com/example/vehiclebreakdownbuddy/splashscreen.java
+++ app/src/main/java/com/example/vehiclebreakdownbuddy/splashscreen.java
@@ -4,15 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splashscreen extends AppCompatActivity {
+
+    ImageView splashImage;
+    TextView splashText;
+    Animation top, bottom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-
-
         setContentView(R.layout.activity_splashscreen);
+
+        splashImage = findViewById(R.id.splashImage);
+        splashText = findViewById(R.id.splashText);
+
+        top = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom);
+
+        splashImage.setAnimation(top);
+        splashText.setAnimation(bottom);
+
+
+
+
         // Set up everything needed for our app's screen
 
         new Handler().postDelayed(new Runnable() {
@@ -24,7 +45,7 @@ public class splashscreen extends AppCompatActivity {
                 // Define what to do after a delay
                 // intent--> it is used to switch from one activity to another activity
                 // Create an 'intent' to switch from the splash activity to the main activity
-                Intent intent = new Intent(splashscreen.this,MainActivity.class);
+                Intent intent = new Intent(splashscreen.this,sginupActivity.class);
 
                 // Tell the app to start the main activity
                 startActivity(intent);
@@ -32,6 +53,6 @@ public class splashscreen extends AppCompatActivity {
                 // Close the splash activity, like turning off a light when we don't need it anymore
                 finish();
             }
-        },3000);  // screen visible for 3 sec
+        },2500);  // screen visible for 3 sec
     }
 }
